db5b1af5873a4d1a1ad607470ab55a4d4e7d0f92
Brian Darrow <brianpdarrow@gmail.com>
2015-08-25 10:22:53 -0400

    Version 2.0

--- Install-PCCSPSolution.ps1
+++ Install-PCCSPSolution.ps1
@@ -1,5 +1,26 @@
 Add-PSSnapin "Microsoft.SharePoint.Powershell"
 
+#Function that allows the enable-SPFeature to wait until the solution timer job properly finishes.
+function WaitForJobToFinish([string]$SolutionFileName)
+{ 
+    $JobName = "*solution-deployment*$SolutionFileName*"
+    $job = Get-SPTimerJob | ?{ $_.Name -like $JobName }
+    if ($job -eq $null) 
+    {
+        Write-Host 'Timer job not found'
+    }
+    else
+    {
+        $JobFullName = $job.Name
+        
+        while ((Get-SPTimerJob $JobFullName) -ne $null) 
+        {
+            Write-Host -NoNewLine .
+            Start-Sleep -Seconds 2
+        }
+    }
+}
+
 $WEBAPP = Read-Host "Enter the site collection URL where the solution will be deployed"
 
 Write-Host "Installing Accusoft.PCC.wsp"
@@ -7,6 +28,13 @@ Add-SPSolution $PSScriptRoot\Accusoft.PCC.wsp
 Install-SPSolution Accusoft.PCC.wsp -GACDeployment -Force -WebApplication $WEBAPP
 
 Write-Host "Enabling Feature"
-Enable-SPFeature "Accusoft.PCC" -URL $WEBAPP -ErrorAction SilentlyContinue
+WaitForJobToFinish("accusoft.pcc.wsp")
+Enable-SPFeature Accusoft.PCC -Url $WEBAPP -ErrorAction SilentlyContinue
+
+#This script allows STS to authenticate users over HTTP.
+Write-Host "Updating STS"
+$config = (Get-SPSecurityTokenServiceConfig)
+$config.AllowOAuthOverHttp = $true
+$config.Update()
 
 Read-Host -Prompt "Script completed.  Press Enter to exit."
--- Update-PCCWOPIBinding.ps1
+++ Update-PCCWOPIBinding.ps1
@@ -3,16 +3,30 @@ Add-PSSnapin "Microsoft.SharePoint.Powershell"
 $WOPIHOST = Read-Host "Enter the host name of the PCC WOPI Client site"
 
 Write-Host "Updating PCC WOPI Bindings"
+
 Set-SPWOPIZone -Zone internal-http -ErrorAction SilentlyContinue
-Remove-SPWOPIBinding -Action view -Confirm:$false -ErrorAction SilentlyContinue
-Remove-SPWOPIBinding -Action interactivepreview -Confirm:$false -ErrorAction SilentlyContinue
+
+$ViewBinding = Get-SPWOPIBinding -Action view
+if($ViewBinding -ne $null)
+{
+    Remove-SPWOPIBinding -Action view -Confirm:$false
+}
+
+$PreviewBinding = Get-SPWOPIBinding -Action interactivepreview
+if($PreviewBinding -ne $null)
+{
+    Remove-SPWOPIBinding -Action interactivepreview -Confirm:$false
+}
+
 New-SPWOPIBinding -ServerName $WOPIHOST -AllowHTTP
 
-Write-Host "Updating Interactive Preview list of supported extensions"
+Write-Host "Updating the Interactive Preview list of supported extensions"
+
 if (!(Test-Path -path 'C:\Program Files\Common Files\microsoft shared\Web Server Extensions\15\TEMPLATE\LAYOUTS\Prizm')) 
 {
-    New-Item 'D:Data' -type directory
+    New-Item 'C:\Program Files\Common Files\microsoft shared\Web Server Extensions\15\TEMPLATE\LAYOUTS\Prizm' -type directory
 }
+
 Get-SPWOPIBinding -Action interactivepreview | Out-File -filepath 'C:\Program Files\Common Files\microsoft shared\Web Server Extensions\15\TEMPLATE\LAYOUTS\Prizm\WOPIExtensions.txt' -Force
 
 Read-Host -Prompt "Script completed.  Press Enter to exit."
